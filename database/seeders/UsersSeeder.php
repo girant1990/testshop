baa9308e132bf037ead10a9fa8c7977ec2201df9
Tigran Hakobyan <girant1990@gmail.com>
2024-10-02 22:35:45 +0400

    Install breeze and auth, create roles table, create seeders for role and user

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@' . config('app.url'),
+            'password' => bcrypt('password'),
+            'role_id' => Role::where('slug', 'admin')->first()->id
+        ]);
+    }
+}
